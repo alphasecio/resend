9236f599e57fb76e126e003e40195583f87dc4e9
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-06-06 10:28:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from resend import Resend
 client = Resend(api_key=os.environ["RESEND_API_KEY"])
 
 # Set up the Streamlit app
-st.title("Send Email")
+st.subheader("Send Email")
 email_from = st.text_input("From:", "")
 email_to = st.text_input("To:", "")
 email_subject = st.text_input("Subject:", "")
@@ -14,12 +14,13 @@ email_body = st.text_input("Body:", "")
 # Send email using Resend
 if st.button("Submit"):
     if not email_from or not email_to or not email_subject or not email_body:
-        st.error('All fields are required. Please provide the missing information...')
+        st.error('Please provide the missing fields.')
     else:
-        client.send_email(
-            to=email_to, 
-            sender=email_from, 
-            subject=email_subject, 
-            text=email_body
-            )
-        st.success(f"Email sent!")
+        with st.spinner():
+            client.send_email(
+                to=email_to, 
+                sender=email_from, 
+                subject=email_subject, 
+                text=email_body
+                )
+            st.success(f"Email sent!")
